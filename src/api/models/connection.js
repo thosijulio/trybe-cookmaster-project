88cd5ece153a3fe52cca0134b3f13fca2857ce63
thosijulio <thosijulio@gmail.com>
2022-04-29 08:46:09 -0300

    adicionando conexão default com o DB

--- src/api/models/connection.js
+++ src/api/models/connection.js
@@ -2,7 +2,8 @@ const { MongoClient } = require('mongodb');
 
 require('dotenv').config();
 
-const { MONGO_DB_URL, DB_NAME } = process.env;
+const MONGO_DB_URL = process.env.MONGO_DB_URL || 'mongodb://127.0.0.1:27017';
+const DB_NAME = process.env.DB_NAME || 'Cookmaster';
 
 const OPTIONS = {
   useNewUrlParser: true,
